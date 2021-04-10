c15e4d7284b685035414eb9867cf38c7c3839135
GrootStudent <borodichalexander97@gmail.com>
2021-04-10 19:31:16 +0300

    feat: implement base registration

--- src/resources/authentication/signup.router.js
+++ src/resources/authentication/signup.router.js
@@ -1,22 +1,11 @@
 const router = require('express').Router();
 const { OK } = require('http-status-codes');
-const User = require('../users/user.model');
 const userService = require('../users/user.service');
 
 router.route('/').post(async (req, res) => {
   try {
-    console.log('body', req.body);
     const { name, email, password } = req.body;
-    const candidate = await userService.getUserByEmail({ email });
-    if (candidate) {
-      return res
-        .status(400)
-        .json({ message: `User with email ${email} already exist` });
-    }
-
-    const user = new User({ name, email, password });
-    await userService.save(user);
-    // await fileService.createDir(new File({user:user.id, name: ''}))
+    await userService.save({ name, email, password });
     res.status(OK).json({ message: 'User was created' });
   } catch (e) {
     console.error(e);
